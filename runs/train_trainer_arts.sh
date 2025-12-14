a16b1a62ebecc8e6b9ba8b9cc99b1d177aa46709
Heejin Do <heejdo@eu-login-06.euler.ethz.ch>
2025-12-14 12:18:52 +0100

    update folder name

--- runs/train_trainer_arts.sh
+++ runs/train_trainer_arts.sh
@@ -1,2 +1,2 @@
 python train_trainer_arts.py --train_batch 4 --valid_batch 4 --test_batch 4 --epoch 15 \
-    --max_tgt_len 166 --output_path "./results_arts/Arts" --data_path data_asap/fold_0 --steps 5000 --model t5-large
+    --max_tgt_len 166 --output_path "./results/Arts" --data_path data_asap/fold_0 --steps 5000 --model t5-large
